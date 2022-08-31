4868419f9b2903b5270c785905b538e632e99163
Takeshi Komori <stubgurssy@gmail.com>
2022-09-01 08:23:51 +0900

    プロジェクト作成

---
+++ XcodeGenSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodeGenSample
+//
+//  Created by Takeshi Komori on 2022/09/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XcodeGenSample/XcodeGenSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGenSampleApp.swift
+//  XcodeGenSample
+//
+//  Created by Takeshi Komori on 2022/09/01.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGenSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
