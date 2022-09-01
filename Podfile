3ce8e246107767bb63a53cbeb949827e984c0d7d
Takeshi Komori <stubgurssy@gmail.com>
2022-09-01 10:30:46 +0900

    cocoapods install して build できるところまで

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target :XcodeGenSample do
+  pod 'lottie-ios'
+end
--- XcodeGenSample/ContentView.swift
+++ XcodeGenSample/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct ContentView: View {
     var body: some View {
